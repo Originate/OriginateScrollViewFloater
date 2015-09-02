c30da8038fc226faab19b23e89e2a75efcb194b9
Allen Wu <allen.wu@originate.com>
2015-09-01 21:59:45 -0700

    added example project

---
+++ Example/OriginateScrollViewFloaterExample/OriginateScrollViewFloaterExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OriginateScrollViewFloaterExample
+//
+//  Created by Allen Wu on 9/1/15.
+//  Copyright (c) 2015 Originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/OriginateScrollViewFloaterExample/OriginateScrollViewFloaterExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OriginateScrollViewFloaterExample
+//
+//  Created by Allen Wu on 9/1/15.
+//  Copyright (c) 2015 Originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
